1e45f6d92ccc78030f37c10258eb6b0583355130
a-poor <a-poor@users.noreply.github.com>
2021-11-16 21:00:04 -0800

    Updating wiggler code

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,80 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
+import cli from 'cli-ux'
+import * as robot from 'robotjs'
+
 
 class WiggleMouse extends Command {
-  static description = 'describe the command here'
+  static description = "A CLI to keep your screen active by moving the mouse around randomly."
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    
+    rate: flags.integer({
+      char: 'r', 
+      description: "Rate of movement in milliseconds",
+      default: 1500,
+    }),
+    click: flags.boolean({
+      char: 'c', 
+      description: "Should the mouse be intermittantly clicked?",
+      default: false,
+    }),
+    clickrate: flags.integer({
+      description: "Probability to click (out of 100) where the mouse lands, if click is true.",
+      default: 100,
+    }),
+    mousepos: flags.boolean({
+      char: 'p',
+      description: "Display the position of the mouse (and don't run the wiggler)",
+      default: false,
+    }),
+    until: flags.integer({
+      char: 'u',
+      required: false,
+      description: "Number of milliseconds to run the wiggler",
+    }),
+    
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(WiggleMouse)
+    const { flags } = this.parse(WiggleMouse)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    console.log(JSON.stringify(flags))
+
+    // Check if the position flag is set
+    if (flags.mousepos) {
+      const { x, y } = robot.getMousePos()
+      this.log(`Mouse position: ${x},${y}`)
+      return
     }
+
+    // Get the screen size
+    const { width, height } = robot.getScreenSize()
+    const startTime = Date.now()
+
+    const clickRate = flags.clickrate || 100
+
+    cli.action.start('Wiggling the mouse...')
+    try {
+      while (flags.until === undefined || Date.now() - startTime < flags.until) {
+        // Move the mouse randomly
+        const x = Math.random() * width
+        const y = Math.random() * height
+        robot.moveMouse(x, y)
+  
+        // Click?
+        if (flags.click && Math.random() * 100 < flags.clickrate) {
+          robot.mouseClick()
+        }
+  
+        // Sleep
+        await new Promise(resolve => setTimeout(resolve, flags.rate))
+      }
+    } finally {
+      cli.action.stop("Done wiggling.")
+    }
+    
   }
 }
 
